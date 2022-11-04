b6f0426999386f898d78519a4ed821a95150ff9b
Jongmin <whdals011203@naver.com>
2022-11-05 02:58:21 +0900

    [구현] 예제 및 실전문제 해결(김종민)

---
+++ 구현/example2.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+cnt = 0
+
+for h in range(0,N+1):
+  for m in range(0,60):
+    for s in range(0,60):
+      if '3' in str(h) or '3' in str(m) or '3' in str(s):
+        cnt += 1
+
+print(cnt)
